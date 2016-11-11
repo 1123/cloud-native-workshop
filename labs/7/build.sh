044d885e0778eba83de5bd1075904335abcaae87
Benedikt Linse <benedikt.linse@emc.com>
2016-11-11 10:28:57 +0100

    adjusting buildscript to use mvnw

--- labs/7/build.sh
+++ labs/7/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+MVN=../../../code/auth-server/mvnw
+
 set -e
 set -u
 
-cd config-service && mvn clean package && cd ..
-cd eureka-service && mvn clean package && cd ..
-cd reservation-service && mvn clean package -Dmaven.test.skip=true && cd ..
-cd reservation-client && mvn clean package -Dmaven.test.skip=true && cd ..
-cd hystrix-dashboard && mvn clean package && cd ..
+cd config-service && $MVN clean package && cd ..
+cd eureka-service && $MVN clean package && cd ..
+cd reservation-service && $MVN clean package -Dmaven.test.skip=true && cd ..
+cd reservation-client && $MVN clean package -Dmaven.test.skip=true && cd ..
+cd hystrix-dashboard && $MVN clean package && cd ..
