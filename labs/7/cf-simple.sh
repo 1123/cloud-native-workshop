bc5d64c251136be894938f74a9594a7b84cbde90
Benedikt Linse <benedikt.linse@emc.com>
2016-11-17 09:33:26 +0100

    Migrating UPSes to https.
    User provided services are now referenced via https.
    Therefore the cloudfoundry certificate truster dependency must be added to the poms
    of all apps that get their configuration from the configuration service.

--- labs/7/cf-simple.sh
+++ labs/7/cf-simple.sh
@@ -2,12 +2,12 @@ cf create-service p-rabbitmq standard message-bus
 
 cd config-service
 cf push
-cf cups config-service -p '{"uri":"http://config-service.local2.pcfdev.io"}'
+cf cups config-service -p '{"uri":"https://config-service.local2.pcfdev.io"}'
 cd ..
 
 cd eureka-service
 cf push
-cf cups eureka-service -p '{"uri":"http://eureka-service.local2.pcfdev.io"}'
+cf cups eureka-service -p '{"uri":"https://eureka-service.local2.pcfdev.io"}'
 cd ..
 
 cd reservation-service
