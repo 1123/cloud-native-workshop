ac7a13c3e26651d11597ca7d317bf3acb04cea9e
Benedikt Linse <benedikt.linse@emc.com>
2016-11-24 14:30:08 +0100

    adding manifest files and CF install script to code directory

---
+++ code/install.sh
@@ -0,0 +1,23 @@
+cf create-service p-rabbitmq standard message-bus
+
+cd config-service
+cf push
+cf cups config-service -p '{"uri":"https://config-service.local2.pcfdev.io"}'
+cd ..
+
+cd eureka-service
+cf push
+cf cups eureka-service -p '{"uri":"https://eureka-service.local2.pcfdev.io"}'
+cd ..
+
+cd reservation-service
+cf push
+cd ..
+
+cd reservation-client
+cf push
+cd ..
+
+cd hystrix-dashboard
+cf push 
+cd ..
